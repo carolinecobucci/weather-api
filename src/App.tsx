143067b6386e8b4e0d5088268d924b7181ba7986
Carol Cobucci <100380628+cobuti@users.noreply.github.com>
2023-07-05 20:22:50 -0300

    introduced api request

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,78 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
+// components
 import NavBar from "./components/NavBar.tsx";
 import City from "./components/City.tsx";
 import Temperature from "./components/Temperature.tsx";
 import ClimateInfo from "./components/ClimateInfo.tsx";
 
+interface WeatherResponse {
+  coord: {
+    lon: number;
+    lat: number;
+  };
+  weather: {
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }[];
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    humidity: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+  };
+  clouds: {
+    all: number;
+  };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+}
+
 function App() {
+  const lat = 44.34;
+  const lon = 10.99;
+  const apiID = "49152378eb34f3c5a0cad3bb944c413b";
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiID}`;
+  const [data, setData] = useState<WeatherResponse | null>(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  console.log(data);
+  //  OPTIONAL CHANING data?. significa que o objeto pode retornar null
   return (
     <div>
       <NavBar />
       <City />
-      <Temperature />
+      <Temperature temperature={data?.main.temp} description={data?.weather[0].description} />
       <ClimateInfo />
     </div>
   );
--- src/components/Temperature.tsx
+++ src/components/Temperature.tsx
@@ -1,13 +1,19 @@
-const Temperature = () => {
+const Temperature = ({
+  temperature,
+  description,
+}: {
+  temperature: number | undefined;
+  description: string | undefined;
+}) => {
   return (
     <div className="flex m-auto w-72 sm:w-80 md:w-96 items-center justify-center mb-6 space-x-4">
       <img src="/src/assets/cludy.svg" alt="temperature icon" />
-      <div className="flex">
-        <div className="flex-columm">
-          <p className="text-5xl font-bold text-[#303345]">19</p>
-          <p className="text-base text-[#303345] ml-2">Rainy</p>
+      <div>
+        <div className="flex">
+          <p className="text-5xl font-bold text-[#303345]">{temperature}</p>
+          <p className="text-sm text-[#303345]">°C</p>
         </div>
-        <p className="text-sm text-[#303345]">°C</p>
+        <p className="text-base text-[#303345] ml-2">{description}</p>
       </div>
     </div>
   );
